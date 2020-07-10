f8d7e3a31ce9ffaa60abb97dc59821fe83b4229b
SimarpreetKaurj <simarkaurj515@gmail.com>
2020-07-09 20:52:11 -0400

    2 files modified, 1 added

---
+++ ArrayOfObjects.js
@@ -0,0 +1,21 @@
+const employeeInfo=[
+  {
+    id:1,
+    name:"Simarpreet",
+    occupation:"Software Developer"
+  },
+  {
+    id:2,
+    name:"Vsu",
+    occupation:"Software Developer"
+  },
+  {
+    id:3,
+    name:"Simrat Kaur",
+    occupation:"Software Tester"
+  }
+]
+console.log("Hello World!")
+
+const jsonFile=JSON.stringify(employeeInfo);
+console.log(jsonFile);
